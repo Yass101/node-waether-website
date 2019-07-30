e90ca4d72f8624424e8cd145b7ba7d4bc502908f
Yasser <knight09090@gmail.com>
2019-07-30 13:19:20 +1200

    add port variable fix fetch url in public/js/app.js

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ const msg2 = document.getElementById('msg2')
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
     const value = searched.value;
-    fetch(`http://localhost:3000/weather?city=${value}`).then((response) => {
+    fetch(`/weather?city=${value}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 return msg1.innerText = `error has occured >> ${data.error}`;
--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
-const geoCode = require('./utlis/geoCode')
-const forecast = require('./utlis/forecast')
+const geoCode = require("./utlis/geoCode");
+const forecast = require("./utlis/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 const publicFolderPath = path.join(__dirname, "../public/");
 const viewsPath = path.join(__dirname, "../templates/views");
@@ -16,82 +17,73 @@ app.use(express.static(publicFolderPath));
 hbs.registerPartials(partialsPath);
 
 app.get("/", (req, res) => {
-    res.render("index", {
-        title: "Weather App",
-        name: "Yass"
-    });
+  res.render("index", {
+    title: "Weather App",
+    name: "Yass"
+  });
 });
 
 app.get("/help", (req, res) => {
-    res.render("help", {
-        title: "Help page",
-        name: "Yass"
-    });
+  res.render("help", {
+    title: "Help page",
+    name: "Yass"
+  });
 });
 
 app.get("/about", (req, res) => {
-    res.render("about", {
-        title: "About page",
-        name: "Yass"
-    });
+  res.render("about", {
+    title: "About page",
+    name: "Yass"
+  });
 });
 
 app.get("/help/*", (req, res) => {
-    res.render("generic", {
-        title: "404",
-        name: "Yass",
-        errorMassege: "Help articales not found bitch"
-    });
+  res.render("generic", {
+    title: "404",
+    name: "Yass",
+    errorMassege: "Help articales not found bitch"
+  });
 });
 
-app.get('/weather', (req, res) => {
-    // console.log(req.query.city);
-    if (!req.query.city) {
-        return res.send({
-            error: "you must provide a city asshole"
-        });
-    }
+app.get("/weather", (req, res) => {
+  // console.log(req.query.city);
+  if (!req.query.city) {
+    return res.send({
+      error: "you must provide a city asshole"
+    });
+  }
 
-    geoCode(req.query.city, (error, {
-        latitude,
-        longitude,
-        location
-    } = {}) => {
+  geoCode(req.query.city, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      res.send({
+        error
+      });
+    } else {
+      forecast(latitude, longitude, (error, forecastData) => {
         if (error) {
-            res.send({
-                error
-            })
+          res.send({
+            error
+          });
         } else {
-            forecast(latitude, longitude, (error, forecastData) => {
-                if (error) {
-                    res.send({
-                        error
-                    })
-                } else {
-                    res.send({
-                        forecast: forecastData,
-                        location,
-                        address: req.query.city
-                    })
-                }
-            })
+          res.send({
+            forecast: forecastData,
+            location,
+            address: req.query.city
+          });
         }
-    })
-    // res.send({
-    //     forecast: "it's raining ",
-    //     location: "Auckland",
-    //     address: req.query.city
-    // })
-})
+      });
+    }
+  });
+});
 
 app.get("*", (req, res) => {
-    res.render("generic", {
-        title: "404",
-        errorMassege: "Page not found deal with it bitch",
-        name: "Yass"
-    });
+  res.render("generic", {
+    title: "404",
+    errorMassege: "Page not found deal with it bitch",
+    name: "Yass"
+  });
 });
 
-app.listen(3000, () => {
-    console.log("Server Started");
+app.listen(port, () => {
+  console.log(`Server Started on port ${port}`);
 });
